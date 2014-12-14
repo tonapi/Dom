66e2495b68a5ea469e5a0113e73720cbe7dc3ca8
tonapi <tnapiorkowski@poczta.fm>
2014-12-14 13:59:35 +0100

    New root view, main index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Dom::Application.routes.draw do
+  get "main/index"
+  root "main#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
