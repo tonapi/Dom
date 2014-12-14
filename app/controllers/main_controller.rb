bb12bb5e1b3630d5a1793b4cc12e70d30cd5031d
tonapi <tnapiorkowski@poczta.fm>
2014-12-14 16:56:37 +0100

    Item added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,23 @@
 class MainController < ApplicationController
+	before_action :prepare_sidebar
+
   def index
   end
+
+  def room
+  	@room =Room.find(params[:id])
+  	end
+
+  def category
+  	@category =Category.find(params[:id])
+  end
+
+  private 
+  def prepare_sidebar
+  	@rooms = Room.all
+  	@categories = Category.all
+
+  end
+
+
 end
