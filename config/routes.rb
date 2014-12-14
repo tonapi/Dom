11db331482d5ebe132d279accf9747eee7cf9a5b
tonapi <tnapiorkowski@poczta.fm>
2014-12-14 15:29:37 +0100

    sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Dom::Application.routes.draw do
+  get "admin/index"
   devise_for :users
+
+  scope '/admin' do
   resources :devices
-
   resources :room_categories
-
   resources :categories
-
   resources :rooms
+end
 
   get "main/index"
   root "main#index"
